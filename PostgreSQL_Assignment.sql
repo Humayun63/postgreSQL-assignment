19925c1a8e2a5485266c5639fa6b0a0318f367d6
kabir-coderex <kabir@coderex.co>
2025-08-17 12:40:58 +0600

    solved problem 4

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -50,3 +50,10 @@ SELECT COUNT(DISTINCT(species_id)) as "unique_species_count" FROM "sightings";
 -- - Problem 3
 SELECT * FROM "sightings"
     WHERE location LIKE '%Pass%';
+
+-- - Problem 4
+SELECT r.name as "name", COUNT(s.sighting_id) as "total_sightings" FROM "rangers" r
+    JOIN "sightings" s
+    ON r.ranger_id = s.ranger_id
+    GROUP BY r.name;
+
